2379b577581257931ac8bf626dc68d6acd05b5e0
lvhquan <lvhquan0110@gmail.com>
2023-03-02 01:17:16 +0700

    data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  public name: string = 'lvhquan';
+  public linkYoutube: string = 'https://www.youtube.com/';
+  public title: string = 'Click go to ytb';
+  public isNonDecoration: boolean = true;
+  public color: string = 'cyan';
+  public count: number = 0;
+  public keyword: string = '';
+  public result: string = '';
+
+  onClick = function(params: any) {
+    console.log(params.target.innerHTML)
+  }
+
+  onClickCount() {
+    this.count++;
+  }
+
+  onKeyUp(params:any) {
+    console.log(params.target.value);
+    this.keyword = params.target.value
+  }
+
+  onKeyUpEnter(event:any) {
+    console.log(event.target.value);
+    this.result = event.target.value;
+  }
 
 }
